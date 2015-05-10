64f877565137facef6700ca1ab723738664aeef1
ghj1976 <ghj1976@hotmail.com>
2015-05-10 21:48:12 +0800

     增加返回参数校验

--- crawler/weibo/weiboCrawler/main.go
+++ crawler/weibo/weiboCrawler/main.go
@@ -15,21 +15,27 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	/*
-		oauth := weibo.WeiboAuth{ClientId: config.AppKey, ClientSecret: config.AppSecret, RedirectUri: "http://www.teaduoduo.com/weibo"}
 
-		jj, err := oauth.GetAccessToken(config.Code)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		config.AccessToken = jj.AccessToken
-	*/
+	// oauth := weibo.WeiboAuth{ClientId: config.AppKey, ClientSecret: config.AppSecret, RedirectUri: "http://www.teaduoduo.com/weibo"}
+
+	// jj, err := oauth.GetAccessToken(config.Code)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// config.AccessToken = jj.AccessToken
 	// fmt.Println(jj.AccessToken)
+
 	tl, err := weibo.GetMyTimeline(config.AccessToken)
 	if err != nil {
 		fmt.Println(err)
 
+		return
+	} else if tl.ErrorCode != 0 {
+		fmt.Println("weibo.GetMyTimeline error")
+		fmt.Println(tl.ErrorCode)
+		fmt.Println(tl.ErrorMsg)
+		fmt.Println(tl.RequestUrl)
 		return
 	}
 
